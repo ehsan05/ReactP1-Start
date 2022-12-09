4263585d565474151d10090e29fa321e87f5f163
Ehsan <ehsanellahi051@gmail.com>
2022-12-09 09:59:56 +0500

    initial commit

---
+++ my-app/src/Component/App.js
@@ -0,0 +1,22 @@
+// import Header from './Header';
+// import Hero from './Hero';
+// import Footer from './Footer';
+import Card from './contact';
+
+function App() {
+  return (
+    <div className="App" >
+      {/* <Header />
+      <Hero />
+      <Footer /> */}
+      <Card 
+        name="Ehsan Ellhai"
+        img=""
+        mail="ehsanellahiofficial@gmail.com"
+        num="+923078607264"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero(){
+    return (
+        <div>
+        <div className='content'>
+           <h1>This is the note title</h1>
+           <p>This is the note content</p> 
+           </div>
+        </div>
+    );
+}
+
+export default Hero;
